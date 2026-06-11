ba11f289c9b4fff32b6ff73ece3f9bb51726d2ed
mateuszmarczakPP <mateusz.marczak@student.put.poznan.pl>
2026-06-11 22:27:14 +0200

    Usuniecie folderu build z repozytorium

--- build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.obj.d
+++
@@ -1,8 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.obj: \
- D:\Space_Shooter\build\Desktop_Qt_6_11_1_MinGW_64_bit-Debug\_deps\sfml-src\src\SFML\Graphics\Vertex.cpp \
- D:/Space_Shooter/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/_deps/sfml-src/include/SFML/Graphics/Vertex.hpp \
- D:/Space_Shooter/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- D:/Space_Shooter/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/_deps/sfml-src/include/SFML/Config.hpp \
- D:/Space_Shooter/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/_deps/sfml-src/include/SFML/Graphics/Color.hpp \
- D:/Space_Shooter/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/_deps/sfml-src/include/SFML/System/Vector2.hpp \
- D:/Space_Shooter/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/_deps/sfml-src/include/SFML/System/Vector2.inl
--- build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Time.cpp.obj.d
+++
@@ -1,5 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Time.cpp.obj: \
- D:\Space_Shooter\build\Desktop_Qt_6_11_1_MinGW_64_bit-Debug\_deps\sfml-src\src\SFML\System\Time.cpp \
- D:/Space_Shooter/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/_deps/sfml-src/include/SFML/System/Time.hpp \
- D:/Space_Shooter/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/_deps/sfml-src/include/SFML/System/Export.hpp \
- D:/Space_Shooter/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/_deps/sfml-src/include/SFML/Config.hpp
